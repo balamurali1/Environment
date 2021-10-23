e4304082b7384cb6e440d52461706752a376e27d
Balamurali <balamurali1@gmail.com>
2021-10-23 14:07:43 +0530

    Model View Set topic and router topic(i am writing very shortcut code vvIMP)

---
+++ Model_View_Set/Book/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from . models import *
+from .serializers import *
+from rest_framework import status
+from rest_framework import viewsets
+
+# Create your views here.
+
+class Studentviewset(viewsets.ModelViewSet):
+	queryset = Student.objects.all() #this is queryset
+	serializer_class=Studentserializer #queryset ni JSON format lo ki conver chesthundi serializers.py file.. 
+
+
+"""
+ModelViewSet lo ne anni APIs actions untai... it is  short cut code..
+
+APIs actions means like .list()-->GET(),
+.crate()-->POST(),
+.update()-->first GET() chesi modify chesi malli send chesetappudu PUT()and PATCH() vadu..,
+.retrieve()-->GET(),retrieve means display particular record(indtance)(objects),
+.destroy()-->DELETE() 
+	evi anni kuda ModelViewSet lo ne untai..
+"""
