589918e687bb7662bed04b3f2214b92c4c99b510
Balamurali <balamurali1@gmail.com>
2021-10-25 20:08:43 +0530

    custom validations,@api_view(['GET','POST','PUT','PATCH', 'DELETE']) Function Based View

--- CRUD_FunctionBased_1/Book/serializers.py
+++ CRUD_FunctionBased_1/Book/serializers.py
@@ -5,3 +5,18 @@ class StudentSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Student
 		fields = "__all__"
+
+
+#Custom Validations	
+	def validate(self, data): #post,update chesthe error ravali ani condition echa..
+		if data['roll'] < 101:
+			raise serializers.ValidationError({'error':"roll cannot be less than 101"})	
+
+		
+		if data['name']: #post,update chesthe error ravali ani condition echa...
+			for n in data['name']:
+				if n.isdigit():
+					raise serializers.ValidationError({'error':"name cannot be numeric"})
+
+		return data	
+			
