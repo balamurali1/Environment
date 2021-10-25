ee31558c069281aaffceeb0a69e37e08c2057a9a
Balamurali <balamurali1@gmail.com>
2021-10-25 22:28:39 +0530

    custom validations,@api_view(['GET','POST','PUT','PATCH', 'DELETE']) topics

--- CRUD_FunctionBased_1/Book/serializers.py
+++ CRUD_FunctionBased_1/Book/serializers.py
@@ -7,16 +7,27 @@ class StudentSerializer(serializers.ModelSerializer):
 		fields = "__all__"
 
 
-#Custom Validations	
-	def validate(self, data): #post,update chesthe error ravali ani condition echa..
+	
+	
+	def validate(self, data): #data is a python "dictionary" of field vales, #post,update chesthe error ravali ani condition echa..
 		if data['roll'] < 101:
 			raise serializers.ValidationError({'error':"roll cannot be less than 101"})	
-
+			
+		if data['roll'] >= 103:
+			raise serializers.ValidationError("Seat Full")	
+	
 		
 		if data['name']: #post,update chesthe error ravali ani condition echa...
 			for n in data['name']:
 				if n.isdigit():
 					raise serializers.ValidationError({'error':"name cannot be numeric"})
 
-		return data	
+		return data	 #annitiki kalipi okate 'return data' ni echanu..
+
+	
+
+
+
+
+
 			
